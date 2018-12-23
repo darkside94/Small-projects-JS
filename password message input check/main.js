a5eff4970b70ac1f88214c8c9ebe0caab8850b31
darkside94 <43117771+darkside94@users.noreply.github.com>
2018-12-23 14:19:34 +0100

    Add files via upload

---
+++ password message input check/main.js
@@ -0,0 +1,24 @@
+// Projekt 5 - Zadanie dla Ciebie 
+// Sprawdź czy wpisane w input hasło pasuje do któegoś z hasel w tablicy. Jeśli tak wyświetl wiadomość w <div>
+//wielkość liter nie ma znaczenia (wykorzystaj metodę .toUpperCase lub .toLowerCase - pamiętaj, że metody te działają na pojedyńczym elemencie typu string)
+
+const input = document.querySelector("input");
+const div = document.querySelector('div')
+const passwords = ['jedEN', 'DwA'];
+const messages = ["super", "działa!"]
+
+const showMessage = (e) => {
+    //tutaj rozwiązanie
+    // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
+    const text = e.target.value;
+    const trans = text.toLowerCase();
+
+    passwords.forEach((password, index) => {
+        if (password.toLowerCase() === trans) {
+            div.textContent = messages[index];
+            e.target.value = '';
+        }
+    })
+}
+
+input.addEventListener("input", showMessage)
